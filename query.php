798685b5f6a1bdee7f9f65bd2a8842a262775816
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2024-01-10 19:00:03 +0100

    commit

---
+++ query.php
@@ -0,0 +1,8 @@
+1. SELECT * FROM studenti WHERE YEAR(date_of_birth) = 1990;
+2. SELECT * FROM courses WHERE cfu > 10;
+3. SELECT * FROM students WHERE YEAR(date_of_birth) < 1994;
+4. SELECT * FROM courses WHERE period = 'I semestre' AND year = 1;
+5. SELECT * FROM exams WHERE hour > "14:01:00";
+6. SELECT * FROM degrees WHERE level = "magistrale";
+7. SELECT COUNT(*) FROM departments;
+8. SELECT * FROM `teachers`WHERE phone is null;
